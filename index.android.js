53d4c8699751184c776c6ed1eb7a41131c66e69d
joaovillalba <joao.villalba@live.com>
2018-08-21 17:06:36 -0300

    App GUI

--- index.android.js
+++ index.android.js
@@ -2,101 +2,33 @@ import React from 'react';
 import { View, Text, AppRegistry, TouchableOpacity, Image } from 'react-native';
 
 const Estilos = {
-  textStyle: {
-    fontSize: 30,
-    backgroundColor: 'aqua',    
-    shadowColor: '#000',
-    shadowOffset: { width: 10, height: 15 },
-    shadowOpacity: 1,
-    textAlign: 'center',
-    padding: 10,    
-    textAlignVertical: 'center',
-  },
-  textStyle2: {
-    fontSize: 19,
-    backgroundColor: '#2A48FA',    
-    shadowColor: '#000',
-    shadowOffset: { width: 10, height: 15 },
-    shadowOpacity: 1,
-    textAlign: 'center',
-    textAlignVertical: 'center',
-    padding: 12,
-    
-  },
-  textStyle3: {
-    fontSize: 12,
+  main: {    
     flex: 1,
-    textAlign: 'center'
-  },
-  viewStyle: {
-    backgroundColor: 'skyblue',
-    //height: '100%',
-    flex: 1,    
     justifyContent: 'center',
-    //alignItems: 'center',
-    flexDirection: 'column',
-    paddingTop: 100
+    alignItems: 'center'
   },
-  viewStyle1: {        
-    flex: 5,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    flexDirection: 'column'    
+  button: {
+    backgroundColor: '#538530',
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    marginTop: 20
   },
-  viewStyle2: {        
-    flex: 5,
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    flexDirection: 'row'
-  },
-  buttonStyle: {
-    backgroundColor: '#48BBEC',
-    borderRadius: 5,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 1
-  },
-  textoBotao: {
+  textButton: {
     color: '#fff',
     fontSize: 16,
-    fontWeight: 'bold',
-    padding: 10,
-    alignSelf: 'center' 
-  },
-  imageStyle: {
-    justifyContent: 'center',
-    width: 200, 
-    height: 200,
-    alignItems: 'center'
+    fontWeight: 'bold'
   }
 };
 
 const App = () => {
-  const { textStyle, 
-          textStyle2, 
-          textStyle3, 
-          viewStyle, 
-          viewStyle1, 
-          viewStyle2, 
-          buttonStyle, 
-          textoBotao,
-          imageStyle } = Estilos;  
+  const { main, button, textButton } = Estilos;  
 
   return (
-    <View style={viewStyle}>
-      <View style={viewStyle1}>
-        <Image style={imageStyle} source={require('./img/uvas.png')} >
-          <Text>Legenda para a foto</Text>
-        </Image>
-        <Text style={textStyle} >Frases do Dia</Text>
-        <Text style={textStyle2} >Sua inspiração diária</Text>
-      </View>
-      <View style={viewStyle2}>        
-        <TouchableOpacity style={buttonStyle}>
-          <Text style={textoBotao}>Clique aqui</Text>
-        </TouchableOpacity>
-      </View>
-      <Text style={textStyle3}>Versão: 1.00</Text>
+    <View style={main}>
+      <Image source={require('./img/logo.png')} />
+      <TouchableOpacity style={button}>
+        <Text style={textButton}>Nova frase</Text>
+      </TouchableOpacity>
     </View>
   );
 };
